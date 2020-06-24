df4cb48927e8d2a107588777e6c73d8dc8c3d6b0
ignacypolak1 <ignacypolak1@gmail.com>
2020-06-24 02:48:22 +0200

    FPU enable section

--- Bare-metal_templates/Stm32f303xE/src/main.c
+++ Bare-metal_templates/Stm32f303xE/src/main.c
@@ -1,7 +1,9 @@
 #include "stm32f3xx.h"
 void SystemInit()
 {
-    
+    //Uncomment this, if you want to enable FPU.
+    //SCB->CPACR |= ((3UL<<10*2) | (3UL<<11*2));
+    //Uncomment this, if you want to enable FPU.
 }
 int main()
 {
